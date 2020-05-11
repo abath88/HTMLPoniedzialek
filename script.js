8241f9c854baa600a7d4f62752869411eb28de7e
Albert Czarnecki <abath@vp.pl>
2020-05-11 15:58:27 +0200

    11.05.2020
    Dodana karuzela

--- script.js
+++ script.js
@@ -13,4 +13,21 @@ function handleMenu(){
 
 window.onload =  () => document.getElementById('animate').setAttribute('class', 'animate');
 
-    
+let plateElements = document.getElementById('plate').childElementCount
+let currBlock = 0
+
+function prev() {
+    currBlock = currBlock - 1
+    if(currBlock < 0) {
+        currBlock = plateElements - 1
+    }
+    document.getElementById('plate').style.left = `calc(-${100*currBlock}vw + ${100*currBlock}px)`;
+}
+
+function next() {
+    currBlock = currBlock + 1
+    if(currBlock >= plateElements) {
+        currBlock = 0
+    }
+    document.getElementById('plate').style.left = `calc(-${100*currBlock}vw + ${100*currBlock}px)`;
+}
